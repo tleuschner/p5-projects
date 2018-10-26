dca79b33cbb34bfc7963c2dda841efd2ed23ec37
Tim Leuschner <tim.leuschner@gmail.com>
2018-10-26 19:08:34 +0200

    reduced DOM operations

--- haltestellen-search/sketch.js
+++ haltestellen-search/sketch.js
@@ -2,6 +2,9 @@ let rnvData;
 let graph;
 let dropdownStart;
 let dropdownEnd;
+let idToName;
+let nameToId;
+
 
 function preload() {
 	rnvData = loadJSON("json/connectedStations.json");
@@ -9,10 +12,24 @@ function preload() {
 
 function setup() {
 	graph = new Graph();
-	dropdownStart = createSelect();
-	dropdownEnd = createSelect();
-	dropdownStart.changed(bfs);
-	dropdownEnd.changed(bfs);
+	dropdownStart = document.getElementById('start');
+	dropdownEnd = document.getElementById('end');
+	dropdownStart.addEventListener("change", () => {
+		console.log("changed");
+		bfs();
+	});
+	dropdownEnd.addEventListener("change", () => {
+		bfs();
+	});
+
+	
+
+	idToName = new Map();
+	nameToId = new Map();
+	for (const connection of rnvData.connections) {
+		nameToId.set(connection.stationName, connection.stationID);
+		idToName.set(connection.stationID, connection.stationName);
+	}
 
 	let stations = rnvData.connections;
 	for (const station of stations) {
@@ -20,28 +37,27 @@ function setup() {
 		let connections = station.connectsTo;
 		let stationNode = new Node(id);
 		graph.addNode(stationNode);
-
+		
 		for (const c of connections) {
 			let connectingStation = c;
 			let connectionNode = graph.getNode(connectingStation);
 			if(connectionNode == undefined) {
 				connectionNode = new Node(connectingStation);
-				dropdownStart.option(idToName(connectionNode.value));
-				dropdownEnd.option(idToName(connectionNode.value));
+				//dropdownStart.option(idToName.get(connectionNode.value));
+				//dropdownEnd.option(idToName.get(connectionNode.value));
 			}
 			graph.addNode(connectionNode);
 			stationNode.addEdge(connectionNode);
 		}
 	}
-
 	noLoop();
 	noCanvas();
 }
 
 function bfs() {
 	graph.reset();
-	let start = graph.setStart(nameToId(dropdownStart.value()));
-	let end = graph.setEnd(nameToId(dropdownEnd.value()));
+	let start = graph.setStart(nameToId.get(dropdownStart.value));
+	let end = graph.setEnd(nameToId.get(dropdownEnd.value));
 
 	let queue = [];
 	start.searched = true;
@@ -75,7 +91,7 @@ function bfs() {
 	let txt = '';
 	for(let i = 0; i < path.length; i++) {
 		let n = path[i];
-		txt += idToName(n.value);
+		txt += idToName.get(n.value);
 		if(i != path.length-1) {
 			txt += ' --> ';
 		}
@@ -84,18 +100,8 @@ function bfs() {
 	createP(txt);
 }
 
-function idToName(id) {
-	for (const station of rnvData.connections) {
-		if(station.stationID == id) {
-			return station.stationName;
-		}
-	}
-}
-
-function nameToId(name) {
-	for (const station of rnvData.connections) {
-		if(station.stationName == name) {
-			return station.stationID;
-		}
+function addDropDownValues(p5Dropdown) {
+	for (const connection of rnvData.connections) {
+		p5Dropdown.option(connection.stationName);
 	}
 }
